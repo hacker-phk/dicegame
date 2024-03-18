5ad9e2648aedcc24b4e6a597f078c1a30aa9d4ab
Hemanthkumar <107429016+hacker-phk@users.noreply.github.com>
2024-03-18 10:02:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ function refresh() {
     var n2 = Math.floor(Math.random() * 6) + 1;
   
     // Update the src attribute of the dice images to display the corresponding dice face
-    document.querySelector(".img1").src = `./images/dice${n1}.png`;
-    document.querySelector(".img2").src = `./images/dice${n2}.png`;
+    document.querySelector(".img1").src = `dice${n1}.png`;
+    document.querySelector(".img2").src = `dice${n2}.png`;
   
     // Determine the winner based on the values of n1 and n2
     if (n1 > n2)
